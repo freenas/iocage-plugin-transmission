022e56815e7f8834e90c7b606d2b4db209d3f4d4
Joshua Ruehlig <joshruehlig@gmail.com>
2017-06-28 14:39:35 -0700

    Update post_install.sh
    use the reload function
    make sed capturing a little more explicit

--- post_install.sh
+++ post_install.sh
@@ -4,13 +4,11 @@ sysrc -f /etc/rc.conf transmission_enable="YES"
 # Start service once to create the files needed
 service transmission start
 sleep 2 # It can take a few seconds.
-service transmission stop
-sleep 2 # If it's still running, our changes won't propagate, as they're lost on restart. This is to be safe.
 
 # Without the whitelist disabled, the user will not be able to access it. They can reenable and manually whitelist their IP's.
 SETTINGS="/usr/local/etc/transmission/home/settings.json"
 echo "Disabling RPC whitelist, you may want to reenable it with the specific IP's you will access transmission with by editing $SETTINGS"
-sed -i '' 's/    "rpc-whitelist-enabled": true,/    "rpc-whitelist-enabled": false,/' $SETTINGS
+sed -i '' -e 's/\([[::space:]]*"rpc-whitelist-enabled":[[:space:]]*\)true,/\1false,/' $SETTINGS
 
 # Start the service
-service transmission start
+service transmission reload
