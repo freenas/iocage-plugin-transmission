5ba2910abbb700333ef467aedc76d612c4d1990c
Martin Wilke <miwi@ixsystems.com>
2018-10-04 16:07:57 +0800

    #44317

--- post_install.sh
+++ post_install.sh
@@ -7,6 +7,9 @@ sleep 2 # It can take a few seconds.
 service transmission stop
 sleep 2 # If it's still running, our changes won't propagate, as they're lost on restart. This is to be safe.
 
+#Create a Download Directory
+mkidr -p /usr/local/etc/transmission/home/Downloads
+
 # Without the whitelist disabled, the user will not be able to access it. They can reenable and manually whitelist their IP's.
 SETTINGS="/usr/local/etc/transmission/home/settings.json"
 echo "Disabling RPC whitelist, you may want to reenable it with the specific IP's you will access transmission with by editing $SETTINGS"
