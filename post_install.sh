d6331f0a74ad44c47025c9e7dd2b94dd7ce6616f
Martin Wilke <miwi@ixsystems.com>
2018-10-12 09:13:17 +0800

    Stick the download dir

--- post_install.sh
+++ post_install.sh
@@ -8,7 +8,7 @@ service transmission stop
 sleep 2 # If it's still running, our changes won't propagate, as they're lost on restart. This is to be safe.
 
 #Create a Download Directory
-mkidr -p /usr/local/etc/transmission/home/Downloads
+mkdir -p /usr/local/etc/transmission/home/Downloads
 
 # Without the whitelist disabled, the user will not be able to access it. They can reenable and manually whitelist their IP's.
 SETTINGS="/usr/local/etc/transmission/home/settings.json"
@@ -16,4 +16,5 @@ echo "Disabling RPC whitelist, you may want to reenable it with the specific IP'
 sed -i '' 's/    "rpc-whitelist-enabled": true,/    "rpc-whitelist-enabled": false,/' $SETTINGS
 
 # Start the service
+transmission_download_dir="/usr/local/etc/transmission/home/Downloads"
 service transmission start
